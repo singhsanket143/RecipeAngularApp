9334e1e2e3362073c173e2419e6ce9a779da8d71
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2018-01-01 02:24:47 +0530

    Core Module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {SignupComponent} from "./auth/signup/signup.component";
-import {SigninComponent} from "./auth/signin/signin.component";
+import {HomeComponent} from "./core/home/home.component";
 
 const appRoutes: Routes =[
-  { path: '', redirectTo : '/recipes', pathMatch: 'full'},
+  { path: '', component: HomeComponent},
+  {path: 'recipes', loadChildren: './recipes/recipe.module#RecipeModule'},
   { path: 'shopping-list', component: ShoppingListComponent}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
 import {AppComponent} from './app.component';
-import {HeaderComponent} from "./header/header.component";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {RecipeService} from "./recipes/recipe.service";
@@ -11,24 +8,23 @@ import {HttpModule} from "@angular/http";
 import {DataStorageService} from "./shared/data-storage.service";
 import {AuthService} from "./auth/auth.service";
 import {AuthGuardService} from "./auth/auth-guard.service";
-import {RecipeModule} from "./recipes/recipe.module";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    RecipeModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
