7b99295e14b72bf88df39ef509cdfc5dc018eb17
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-31 16:24:10 +0530

    auth integrated with firebase db

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,19 +3,22 @@ import {Http, Response} from "@angular/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
 import 'rxjs/Rx';
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService) {
+  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {
   }
 
   storerecipe() {
-    return this.http.put('https://angularrecipebook-af9f6.firebaseio.com/recipe.json', this.recipeService.getRecipes());
+    const token=this.authService.getToken();
+    return this.http.put('https://angularrecipebook-af9f6.firebaseio.com/recipe.json?auth='+token, this.recipeService.getRecipes());
   }
 
   getRecipe() {
-    this.http.get('https://angularrecipebook-af9f6.firebaseio.com/recipe.json').map(
+    const token=this.authService.getToken();
+    this.http.get('https://angularrecipebook-af9f6.firebaseio.com/recipe.json?auth='+token).map(
       (response: Response) => {
         const recipes: Recipe[] = response.json();
         for (let recipe of recipes) {
