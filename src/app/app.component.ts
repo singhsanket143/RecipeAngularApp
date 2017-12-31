5b56dc872bc8404547d989a883fa7e05a524178e
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-31 15:12:31 +0530

    firebase config added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   loadedFeature: string = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB-Z8CDc_5_1E5wqxE3rH0e2nPmR1_0S2Y",
+      authDomain: "angularrecipebook-af9f6.firebaseapp.com",
+    });
   }
 }
