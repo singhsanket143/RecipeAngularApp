957669c36944ac42d4c2b96bf890c1abb7a3bf1c
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-31 14:20:22 +0530

    error handled

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
@@ -14,10 +15,19 @@ export class DataStorageService {
   }
 
   getRecipe() {
-    this.http.get('https://angularrecipebook-af9f6.firebaseio.com/recipe.json').subscribe(
+    this.http.get('https://angularrecipebook-af9f6.firebaseio.com/recipe.json').map(
       (response: Response) => {
-        const recipe: Recipe[] = response.json();
-        console.log(recipe);
+        const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe['ingredients']) {
+            recipe['ingredients'] = [];
+
+          }
+        }
+        return recipes;
+      }
+    ).subscribe(
+      (recipe: Recipe[]) => {
         this.recipeService.setRecipes(recipe);
       }
     );
