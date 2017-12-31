a2544214b4de8177678cb5d15abee161cb116374
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-31 23:25:58 +0530

    Auth module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,31 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {RecipeService} from "./recipes/recipe.service";
 import {HttpModule} from "@angular/http";
 import {DataStorageService} from "./shared/data-storage.service";
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from "./auth/auth.service";
 import {AuthGuardService} from "./auth/auth-guard.service";
-import {FormsModule} from "@angular/forms";
 import {RecipeModule} from "./recipes/recipe.module";
-import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
 import {SharedModule} from "./shared/shared.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule,
     RecipeModule,
-    SharedModule
+    SharedModule,
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
